d46f4e27ed94187d9c584f1f33d3a96280abb075
Nihitha8819 <160400642+Nihitha8819@users.noreply.github.com>
2026-07-23 15:18:38 +0530

    Add files via upload

---
+++ module 5/lab-3/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Lab3_Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EF Core Migration Created Successfully.");
+            Console.WriteLine("Database Updated Successfully.");
+            Console.WriteLine("Tables 'Products' and 'Categories' are ready.");
+        }
+    }
+}
